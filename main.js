093435064adf73cdf2a3b866c437ceffba942071
federicosimone <federico.simone15@gmail.com>
2025-11-05 21:17:34 +0100

    utente fa scelta

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 //chiedo all'utente se puntare su pari o dispari
+
+
 //l'utente può scegliere un numero da 1 a 5 
 //il computer genera un numero random usando una funzione
 //fai la somma dei due numeri
 //la somma dei due numeri restituisce un numero pari o dispari? 
 //dichiarare vincitore 
+
+
+//input
+
+let scommessaUtente = prompt('Pari o dispari?');
+console.log(`L'utente ha scelto ${scommessaUtente}`);
+
+ if (scommessaUtente != "pari" && scommessaUtente != "dispari") {
+    alert("Errore! Scegli pari o dispari") ;
+ }
+
+//elaborazione
+//Output
