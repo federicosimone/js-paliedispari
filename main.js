f977620d4524ecceb821e4ed5d745715324aa3d6
federicosimone <federico.simone15@gmail.com>
2025-11-05 21:06:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//chiedo all'utente se puntare su pari o dispari
+//l'utente può scegliere un numero da 1 a 5 
+//il computer genera un numero random usando una funzione
+//fai la somma dei due numeri
+//la somma dei due numeri restituisce un numero pari o dispari? 
+//dichiarare vincitore 
